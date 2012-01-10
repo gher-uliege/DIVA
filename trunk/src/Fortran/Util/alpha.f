e3601a5976077e540ffb767e492e66b8e82c8697
Charles Troupin <charles.troupin@gmail.com>
2012-01-10 21:22:02 +0000

    test deleting

--- trunk/src/Fortran/Util/alpha.f
+++
@@ -1,9 +0,0 @@
-        xi=1
-        
-        read(5,*,END=99) RL, xi
- 99     continue
-        write(12,*) 1/RL**4
-        write(12,*) 2*xi/RL**2
-        stop
-        end
-        
--- trunk/src/Fortran/Util/lcelem.f
+++
@@ -1,6 +0,0 @@
-        read(5,*) ISDENS,RL
-        write(11,*) ISDENS
-        write(11,*) RL/5.
-        stop
-        end
-        
