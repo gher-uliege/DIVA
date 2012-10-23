1947a0d83d86482adf9126cbe3f4421f2e3d558b
root <gher.ulg@gmail.com>
2012-10-23 20:57:25 +0000

    version adaptée de divatedrend

--- DIVA3D/src/Fortran/Util/detrend.f
+++ DIVA3D/src/Fortran/Util/detrend.f
@@ -7,7 +7,7 @@ C First; data index, second: class index, third: group index
        integer*4 indi(ig),ndata,iclm(ig),nd
        real*4 rm(icl,ig),wm(icl,ig)
        real*8 rms1,rms2,rms3
-     
+       integer*4 IP(IG)
        
        character*12 cn
        character*1 groupe
@@ -16,11 +16,30 @@ C First; data index, second: class index, third: group index
        rms3=0
        read(5,*) MG
        write(6,*) 'Number of groups',MG
+       
+       
+       
        if(MG.GT.IG) then
        write(6,*) 'Too many classes, decrease input classes'
        write(6,*) ' or increase IG in detrend.f and recompile'
        stop
        endif
+       
+       read(56,*,ERR=4414,END=4414) (IP(i),i=1,MG)
+       GOTO 445
+ 4414   continue
+       write(6,*) 'Severe error, detrend.order corrupted'
+       write(6,*) 'I will try to recover by using columns 5 6 7 etc'
+       do ii=1,MG
+       IP(ii)=ii+4
+       enddo
+ 445   continue
+       do ii=1,MG
+       IP(ii)=IP(ii)-4
+       enddo
+       
+       
+       write(6,*) 'Order',(IP(ki),ki=1,MG)
 C make a 2 pass algo
        do j=1,ig
        do i=1,icl
@@ -29,17 +48,35 @@ C make a 2 pass algo
        wm(i,j)=0
        enddo
        enddo
-       do k=1,MG
+       do k=1,IG
        iclm(k)=0
        enddo
        ndata=0
+       MGM=1
+C Calculate maximum columns MGM
+       do ii=1,MG
+       MGM=max(MG,IP(ii))
+       enddo
+       if(MGM.GT.IG) then
+       write(6,*) 'Too many classes, decrease input classes'
+       write(6,*) ' or increase IG in detrend.f and recompile'
+       stop
+       endif
+       
+       
+       
  1     continue
-       read(88,*,end=99,err=99) x,y,dd,ww,(indi(k),k=1,MG)
+       read(88,*,end=99,err=99) x,y,dd,ww,(indi(k),k=1,MGM)
        read(89,*,end=99,err=99) x,y,dda
        ndata=ndata+1
-       do k=1,MG
+       do k=1,MGM
+       iclm(k)=max(iclm(k),indi(k))
+C       write(6,*) k,iclm(k),indi(k)
+       enddo
+       do ki=1,MG
+       k=IP(ki)
        ii=indi(k)
-       iclm(k)=max(iclm(k),ii)
+       
 C       write(6,*) k,iclm(k)
        ic(ii,k)=ic(ii,k)+1
        ipoint(ic(ii,k),ii,k)=ndata
@@ -54,10 +91,13 @@ C       write(6,*) k,iclm(k)
 C now subtract
        write(6,*) 'Summary'
        write(6,*) 'Data',ndata
-       write(6,*) 'Groups',MG
-       write(6,*) 'Classes',(iclm(k),k=1,MG)
+       write(6,*) 'Groups',MG,MGM
+       write(6,*) 'Classes',(iclm(k),k=1,MGM)
+       
+C Now do the ordering thing
 C first group
-       do k=1,MG
+       do ki=1,MG
+       k=IP(ki)
        ijmclass=iclm(k)
        do i=1,iclm(k)
        
@@ -109,10 +149,10 @@ C now next group
 
        nd=0
  11     continue
-       read(88,*,end=999,err=999) x,y,dd,ww,(indi(k),k=1,MG)
+       read(88,*,end=999,err=999) x,y,dd,ww,(indi(k),k=1,MGM)
        nd=nd+1
        rms2=rms2+ww*(d(nd)-da(nd))*(d(nd)-da(nd))
-       write(90,444) x,y,d(nd),ww,(indi(k),k=1,MG)
+       write(90,444) x,y,d(nd),ww,(indi(k),k=1,MGM)
        goto 11
  999    continue
  444   format(4(1X,E16.5),5(1X,I6))
